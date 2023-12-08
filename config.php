4750e148782e3ccf22a0d56263af7e8822da7ea8
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-08 21:14:01 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("AZURE_MYSQL_HOST = phpmyadmin.mysql.database.azure.com","AZURE_MYSQL_USERNAME = xukfmicjwi","AZURE_MYSQL_DBNAME = crud","crud") or die("Connection Failed");
+$conn = mysqli_connect("AZURE_MYSQL_HOST = phpmyadmin.mysql.database.azure.com","AZURE_MYSQL_USERNAME = xukfmicjwi","AZURE_MYSQL_PASSWORD = J7C1IY4H0F8TWH06$","AZURE_MYSQL_DBNAME = crud") or die("Connection Failed");
 
 ?>
 
