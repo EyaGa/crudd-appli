8178f51efe4e83fb9148138cb62d96c0e6b34648
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-08 21:00:07 +0100

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -4,7 +4,7 @@
  $stu_class = $_POST['class'];
  $stu_phone = $_POST['sphone'];
 
-$conn = mysqli_connect("localhost:3306","root","","crud") or die("Connection Failed");
+$conn = mysqli_connect("phpmyadmin.mysql.database.azure.com","xukfmicjwi","J7C1IY4H0F8TWH06$","crud") or die("Connection Failed");
 
 $sql = "INSERT INTO student(sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
