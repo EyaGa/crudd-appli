3964f418f6672038cd049edb1ffeabb774d45b39
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-14 00:38:24 +0100

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -4,12 +4,15 @@
  $stu_class = $_POST['class'];
  $stu_phone = $_POST['sphone'];
 
-$conn = mysqli_connect("AZURE_MYSQL_HOST = phpmyadmin.mysql.database.azure.com","AZURE_MYSQL_USERNAME = xukfmicjwi","AZURE_MYSQL_PASSWORD = J7C1IY4H0F8TWH06$","AZURE_MYSQL_DBNAME = crud") or die("Connection Failed");
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+$conn = mysqli_connect('127.0.0.1', 'nadia', 'eYa2096.', 'crud', 3307) or die("Connection Failed");
+mysqli_close($conn);
 
 $sql = "INSERT INTO student(sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 
-header("Location: http://52.146.10.37:80/crud/index.php");
+header("Location: http://20.119.16.24:80/cruddy/index.php");
 
 mysqli_close($conn);
 
