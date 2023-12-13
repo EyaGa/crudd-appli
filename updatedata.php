583bcf13a9611e80ba539b8c913882bfa323d63d
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-14 00:38:50 +0100

    Update updatedata.php

--- updatedata.php
+++ updatedata.php
@@ -10,7 +10,7 @@ include 'config.php';
 $sql = "UPDATE student SET sname = '{$stu_name}', saddress = '{$stu_address}',sclass = '{$stu_class}', sphone = '{$stu_phone}' WHERE sid = {$stu_id}";
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 
-header("Location: http://52.146.10.37:80/crud/index.php");
+header("Location: http://20.119.16.24:80/cruddy/index.php");
 
 mysqli_close($conn);
 
