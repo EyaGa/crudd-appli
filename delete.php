158c553e76d192a171fd34b137996e8c93c0a9d7
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-14 00:39:24 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -8,7 +8,7 @@ $stu_id = $_POST['sid'];
 $sql = "DELETE FROM student WHERE sid = {$stu_id}";
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 
-header("Location: http://52.146.10.37:80/crud/index.php");
+header("Location: http://20.119.16.24:80/cruddy/index.php");
 
 mysqli_close($conn);
 
